d3a502de8021fdceaecc68f698a2732a9254f7ef
668Name <66688@example.com>
2020-03-09 17:25:31 -0600

    Added

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,21 @@ form.addEventListener('submit',function(event){
     `+newTask.value+`
     </li>`;
 
+
+
+var newCheckboxes =document.querySelectorAll('ul [type="checkbox"]');
+for(var i=0;i<newCheckboxes.length;i++)
+{
+    var newCheckbox=newCheckboxes[i];
+
+    newCheckbox.addEventListener('click',function(event){
+    var li=this.parentNode;
+
+    li.removeChild(this);
+    completedList.appendChild(li);
+
+
 });
+}
 
-
+});
